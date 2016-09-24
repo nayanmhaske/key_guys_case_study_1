865521037d0ae3ff25e2f362416c46043acfa42a
nayanmaske <nynmhaske@gmail.com>
2016-09-24 21:55:32 +1000

    This first commit to upload project to GitHub

---
+++ app/src/main/java/com/example/nayan/keyguyscasestudy/KeyGuys_Home.java
@@ -0,0 +1,13 @@
+package com.example.nayan.keyguyscasestudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KeyGuys_Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_key_guys__home);
+    }
+}
